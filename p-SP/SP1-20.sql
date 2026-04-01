8a85c91d4d6ae36d0d98c2dc80127eccc58374aa
Aditya Sahoo <sahooaditya2003@gmail.com>
2026-04-01 11:15:10 +0530

    Add new spv1

--- p-SP/SP1-20.sql
+++ p-SP/SP1-20.sql
@@ -1,7 +1,8 @@
 -- 1- Create a stored procedure to fetch all employees.
 Create Procedure spgetallemp
 as
-begin		
+begin
+SET NOCOUNT ON -- It use to hide like this type of messages (5 rows affected) 
 Select * from Employees;
 end
 
