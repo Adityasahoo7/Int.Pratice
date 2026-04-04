9e2c899a4396be6cbd5187dd8519407f66efccdf
Aditya Sahoo <sahooaditya2003@gmail.com>
2026-04-04 20:06:54 +0530

    getempbyid

--- p-SP/SP1-20.sql
+++ p-SP/SP1-20.sql
@@ -1,10 +1,26 @@
 -- 1- Create a stored procedure to fetch all employees.
 Create Procedure spgetallemp
+@Name VARCHAR(50)
 as
 begin
 SET NOCOUNT ON -- It use to hide like this type of messages (5 rows affected) 
-Select * from Employees;
+Declare @SQL NVARCHAR(Max)
+set @SQL = 'Select * from '+ QUOTENAME(@Name);
+Exec sp_executesql @SQL;
 end
 
-Exec spgetallemp 
+Exec spgetallemp 'Employees';
+
+-- 2- Get employee details by EmpId
+
+Create Procedure spgetempbyid
+@empid int
+as
+begin
+Select * from Employees
+Where EmpId = @empid
+end;
+
+exec spgetempbyid
+@empid =3;
 
