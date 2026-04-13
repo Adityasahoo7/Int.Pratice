a313b381c5db40e1e62e30a40e31639eaeec82a1
Aditya Sahoo <sahooaditya2003@gmail.com>
2026-04-13 11:58:41 +0530

    spincrement

--- p-SP/SP 21-40.sql
+++ p-SP/SP 21-40.sql
@@ -1,1 +1,14 @@
 Select * from Employees
+
+--21 -Increase salary by percentage for a specific department
+create procedure spincrementsalarybydept
+@percent int ,
+@deptid int
+as
+begin
+set nocount on
+Update Employees
+set Salary= Salary + (Salary * @percent/100)
+Where DeptId =@deptid
+end
+
